709428b2ef48d9647e17d5771cc0da4154c0fb3a
Antony Fox-Bramwell <antonyf@mailbox.org>
2025-08-17 14:52:05 +0100

    add theme switcher

---
+++ script.js
@@ -0,0 +1,31 @@
+//// Minor bits and bobs for antonyfb.com
+//
+
+// Theme switcher
+
+function makeLight() {
+    document.body.style.backgroundColor = '#fbf1c7';
+    document.body.style.color = '#282828';
+    localStorage.afTheme = 'light';
+}
+
+function makeDark() {
+    document.body.style.backgroundColor = '#282828';
+    document.body.style.color = '#ebdbb2';
+    localStorage.afTheme = 'dark';
+}
+
+if (localStorage.afTheme === 'light') {
+    makeLight();
+}
+
+function changeTheme() {
+    if (localStorage.afTheme === 'light') {
+	makeDark();
+    } else {
+	makeLight();
+    }
+}
+
+const themeButton = document.querySelector('#themeButton');
+themeButton.addEventListener("click", changeTheme);
